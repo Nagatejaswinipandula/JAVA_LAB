c431bcad8f288f8b20770e3ae2029438ea90918e
Pandula Nagatejaswini <11239A067@kanchiuniv.ac.in>
2025-11-04 09:59:06 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,42 @@
+class Student {
+    String name;
+    int rollNumber;
+    double marks;
+
+    // Constructor
+    Student(String name, int rollNumber, double marks) {
+        this.name = name;
+        this.rollNumber = rollNumber;
+        this.marks = marks;
+    }
+
+    // Method to display student info
+    void displayInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Roll Number: " + rollNumber);
+        System.out.println("Marks: " + marks);
+    }
+
+    // Method to calculate grade based on marks
+    void calculateGrade() {
+        char grade;
+        if (marks >= 90) grade = 'A';
+        else if (marks >= 75) grade = 'B';
+        else if (marks >= 50) grade = 'C';
+        else grade = 'F';
+
+        System.out.println("Grade: " + grade);
+    }
+
+    public static void main(String[] args) {
+        Student s1 = new Student("Alice", 101, 87.5);
+        Student s2 = new Student("Bob", 102, 45.0);
+
+        s1.displayInfo();
+        s1.calculateGrade();
+        System.out.println();
+
+        s2.displayInfo();
+        s2.calculateGrade();
+    }
+}
