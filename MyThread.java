41248a85be2ab405f6c9cb7873e1f2ba04f08da9
Pandula Nagatejaswini <11239A067@kanchiuniv.ac.in>
2025-11-04 10:17:55 +0530

    Add files via upload

---
+++ MyThread.java
@@ -0,0 +1,25 @@
+class MyThread extends Thread {
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(Thread.currentThread().getName() + " : " + i);
+            try {
+                Thread.sleep(500); // Pause for half a second
+            } catch (InterruptedException e) {
+                System.out.println(e);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        MyThread t1 = new MyThread();
+        MyThread t2 = new MyThread();
+
+        t1.setName("Thread-A");
+        t2.setName("Thread-B");
+
+        t1.start(); // starts first thread
+        t2.start(); // starts second thread
+
+        System.out.println("Main thread finished");
+    }
+}
